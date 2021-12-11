91f17465acf362b8d6c9d46fd07965344ae8bc96
yourboyfriendsdrug <74487159+yourboyfriendsdrug@users.noreply.github.com>
2021-12-11 17:47:20 +0300

    .

--- PS213020_Server.BussinessLogic/Services/UserService.cs
+++ PS213020_Server.BussinessLogic/Services/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using PS213020_Server.BussinessLogic.Core.Interfaces;
 using PS213020_Server.BussinessLogic.Core.Models;
 using PS213020_Server.DataAccess.Core.Interface.Context;
@@ -7,45 +8,112 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
+using PS213020_Server.Shared.Exception;
 
 namespace PS213020_Server.BussinessLogic.Services
 {
     public class UserService : IUserService
     {
         private readonly IMapper _mapper;
-        private readonly IRubicContext context;
+        private readonly IRubicContext _context;
 
         public UserService(IMapper mapper, IRubicContext context)
         {
             _mapper = mapper;
-            this.context = context;
+            this._context = context;
         }
-
-        public Task<UserInformationBlo> Auth(int phoneNumberPrefix, int phoneNumber, string password)
+        /// <summary>
+        /// Авторизация пользователя в системе
+        /// </summary>
+        /// <param name="phoneNumberPrefix">Код страны телефона</param>
+        /// <param name="phoneNumber">Номер телефона</param>
+        /// <param name="password">Пароль</param>
+        /// <returns></returns>
+        public async Task<UserInformationBlo> Auth(int phoneNumberPrefix, int phoneNumber, string password)
         {
-            throw new NotImplementedException();
-        }
+           UserRto user = await _context.Users.FirstOrDefaultAsync(x => x.PhoneNumberPrefix == phoneNumberPrefix && x.PhoneNumber == phoneNumber && x.Password == password);
 
-        public Task<bool> DoesExist(int phoneNumberPrefix, int phoneNumber)
+            if (user == null)
+                throw new NotFoundExceptions("Неверный логин или пароль");
+
+            return await ConvertToUserInformationBlo(user);
+            
+        }
+        /// <summary>
+        /// Существует ли пользоваьель
+        /// </summary>
+        /// <param name="phoneNumberPrefix">Код страны номера телефона</param>
+        /// <param name="phoneNumber">Номер телефона</param>
+        /// <returns></returns>
+        public async Task<bool> DoesExist(int phoneNumberPrefix, int phoneNumber)
         {
-            throw new NotImplementedException();
+            return await _context.Users.AnyAsync(x => x.PhoneNumberPrefix == phoneNumberPrefix && x.PhoneNumber == phoneNumber);
         }
-
-        public Task<UserInformationBlo> Get(int UserId)
+        /// <summary>
+        /// Возвращает пользователя
+        /// </summary>
+        /// <param name="UserId">ID</param>
+        /// <returns></returns>
+        public async Task<UserInformationBlo> Get(int UserId)
         {
-            throw new NotImplementedException();
-        }
+            UserRto user = await _context.Users.FirstOrDefaultAsync(x => x.Id == UserId);
 
-        public Task<UserInformationBlo> Registration(int phoneNumberPrefix, int phoneNumber, string password)
+            if (user == null)
+                throw new NotFoundExceptions("Не найден id");
+
+            return await ConvertToUserInformationBlo(user);
+        }
+        /// <summary>
+        /// Регистрация
+        /// </summary>
+        /// <param name="phoneNumberPrefix">Код страны номера телефона</param>
+        /// <param name="phoneNumber">Номер телефона</param>
+        /// <param name="password">Пароль</param>
+        /// <returns></returns>
+        public async Task<UserInformationBlo> Registration(int phoneNumberPrefix, int phoneNumber, string password)
         {
-            throw new NotImplementedException();
-        }
+            UserRto newUser = new UserRto();
+            newUser.PhoneNumberPrefix = phoneNumberPrefix;
+            newUser.PhoneNumber = phoneNumber;
+            newUser.Password = password;
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+            return await ConvertToUserInformationBlo(newUser);
 
-        public Task<UserInformationBlo> Update(int phoneNumberPrefix, int phoneNumber, string password, UserUpdateBlo userUpdateBlo)
+
+        }
+        /// <summary>
+        /// Обновление данных
+        /// </summary>
+        /// <param name="phoneNumberPrefix">Код страны номера телефона</param>
+        /// <param name="phoneNumber">Номер телефона</param>
+        /// <param name="password">Пароль</param>
+        /// <param name="userUpdateBlo">Обновление</param>
+        /// <returns></returns>
+        public async Task<UserInformationBlo> Update(int phoneNumberPrefix, int phoneNumber, string password, UserUpdateBlo userUpdateBlo)
         {
-            throw new NotImplementedException();
-        }
+            UserRto user = await _context.Users.FirstOrDefaultAsync(x => x.PhoneNumberPrefix == phoneNumberPrefix && x.PhoneNumber == phoneNumber && x.Password == password);
 
+            if (user == null)
+                throw new NotFoundExceptions("Пользователь не найден");
+
+            if (userUpdateBlo.IsBoy != null) user.IsBoy = userUpdateBlo.IsBoy;
+            if (userUpdateBlo.Password != null) user.Password = userUpdateBlo.Password;
+            if (userUpdateBlo.FirstName != null) user.FirstName = userUpdateBlo.FirstName;
+            if (userUpdateBlo.LastName != null) user.LastName = userUpdateBlo.LastName;
+            if (userUpdateBlo.Patronymic != null) user.Patronymic = userUpdateBlo.Patronymic;
+            if (userUpdateBlo.Birthday != null) user.Birthday = userUpdateBlo.Birthday;
+            if (userUpdateBlo.AvatarUrl != null) user.AvatarUrl = userUpdateBlo.AvatarUrl;
+
+            await _context.SaveChangesAsync();
+            return await ConvertToUserInformationBlo(user);
+        }
+        /// <summary>
+        /// Конвертирует из UserRto в UserInformationBlo
+        /// </summary>
+        /// <param name="userRto"></param>
+        /// <returns></returns>
         private async Task<UserInformationBlo> ConvertToUserInformationBlo(UserRto userRto)
         {
             
