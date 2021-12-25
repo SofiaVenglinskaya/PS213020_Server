8acc0b82b8c0729db10c82013e2f8d991d7659ab
yourboyfriendsdrug <74487159+yourboyfriendsdrug@users.noreply.github.com>
2021-12-25 17:51:37 +0300

    Server

---
+++ PS213020_Server/AutomapperProfile/MicroserviceProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using PS213020_Server.BussinessLogic.Core.Models;
+using PS213020_Server.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PS213020_Server.AutomapperProfile
+{
+    public class MicroserviceProfile : Profile
+    {
+        public MicroserviceProfile()
+        {
+            CreateMap<UserInformationBlo, UserInformationDto>();
+        }
+    }
+}
--- PS213020_Server/Startup.cs
+++ PS213020_Server/Startup.cs
@@ -2,14 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PS213020_Server.AutomapperProfile;
+using PS213020_Server.BussinessLogic.AutoMapperProfile;
+using PS213020_Server.DataAccess.Core.Interface.Context;
+using PS213020_Server.DataAccess.DbContext;
 
 namespace PS213020_Server
 {
@@ -26,6 +33,8 @@ namespace PS213020_Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddAutoMapper(typeof(BussinessLogicProfile), typeof(MicroserviceProfile));
+            services.AddDbContext<IRubicContext, RubicContext>(o => o.UseSqlite("Data Source=usersdata.db; Foreign Keys=True"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,11 +45,24 @@ namespace PS213020_Server
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor
+                | ForwardedHeaders.XForwardedProto
+            });
+
             app.UseAuthorization();
+
+            using var scope = app.ApplicationServices.CreateScope();
+
+            var mapper = scope.ServiceProvider.GetRequiredService<IMapper>();
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+
+            var dbContext = scope.ServiceProvider.GetRequiredService<RubicContext>();
+            dbContext.Database.Migrate();
 
             app.UseEndpoints(endpoints =>
             {
